f629aaf7b74bc8866745e4e916056bb3310a1f51
Ashwin Sreejith <ashwin.sreejith@gmail.com>
2023-05-08 02:10:25 +1000

    converted attributes to properties

--- movie.py
+++ movie.py
@@ -6,6 +6,22 @@ class Movie:
         self._movie_name = movie_name
         self._seats_available = seats_available
 
+    @property
+    def movie_name(self):
+        return self._movie_name
+
+    @movie_name.setter
+    def movie_name(self, movie_name):
+        self._movie_name = movie_name
+
+    @property
+    def movie_id(self):
+        return self._movie_id
+
+    @movie_id.setter
+    def movie_id(self, movie_id):
+        self._movie_id = movie_id
+
     def display_info(self):
         print(f"Movie ID : {self._movie_id}\n" + f"Movie Name : {self._movie_name}\n" +
               f"Seats available : {self._seats_available}")
