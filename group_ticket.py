231dfd32f3faf63223a582fc50fe9bb3f23ef193
Ashwin Sreejith <ashwin.sreejith@gmail.com>
2023-05-21 00:06:58 +1000

    New ticket type - Group ticket, added

---
+++ group_ticket.py
@@ -0,0 +1,20 @@
+from ticket import Ticket
+
+
+class GroupTicket(Ticket):
+
+    def __init__(self, ticket_id, ticket_name, ticket_price, **kwargs):
+        super().__init__(ticket_id, ticket_name, ticket_price)
+        self.ticket_components = kwargs
+
+    @property
+    def ticket_components(self):
+        return self._ticket_components
+
+    @ticket_components.setter
+    def ticket_components(self, component: dict):
+        self._ticket_components = component
+
+
+
+
