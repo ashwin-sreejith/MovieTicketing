71a83b08afb40d3f643c3ed42bfbdfafb626703f
Ashwin Sreejith <ashwin.sreejith@gmail.com>
2023-05-22 15:13:43 +1000

    Added a counter to track the customer ID number

--- customer.py
+++ customer.py
@@ -1,6 +1,8 @@
 class Customer:
     """ Customer parent class, handles all customer-centric computing"""
 
+    _CUSTOMER_ID_COUNT: int = 0
+
     # Constructor to instantiate the objects
     def __init__(self, customer_id, customer_name):
         # attributes are non-public
@@ -21,7 +23,11 @@ class Customer:
 
     @customer_id.setter
     def customer_id(self, customer_id):
-        self._customer_id = customer_id
+        if len(customer_id) > 1:
+            self._customer_id = customer_id
+        else:
+            self._customer_id = customer_id + str(Customer._CUSTOMER_ID_COUNT)
+            Customer._CUSTOMER_ID_COUNT += 1
 
     # calculates discount for customer
     def get_discount(self, cost: float):
@@ -31,6 +37,14 @@ class Customer:
     @staticmethod
     def get_booking_fee(ticket_quantity: int):
         return 2 * ticket_quantity
+
+    @staticmethod
+    def get_customer_id_count():
+        return Customer._CUSTOMER_ID_COUNT
+
+    @staticmethod
+    def set_customer_id_count(customer_id_number: int):
+        Customer._CUSTOMER_ID_COUNT = customer_id_number
 
     # displays customer information
     def display_info(self):
