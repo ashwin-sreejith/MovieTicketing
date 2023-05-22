85439e2a21c90e07b18cb83b399e0344285b8b7a
Ashwin Sreejith <ashwin.sreejith@gmail.com>
2023-05-22 15:15:51 +1000

    converted component ticket argument from **kwargs to dictionary

--- group_ticket.py
+++ group_ticket.py
@@ -3,9 +3,9 @@ from ticket import Ticket
 
 class GroupTicket(Ticket):
 
-    def __init__(self, ticket_id, ticket_name, ticket_price, **kwargs):
+    def __init__(self, ticket_id, ticket_name, ticket_price, tickets):
         super().__init__(ticket_id, ticket_name, ticket_price)
-        self.ticket_components = kwargs
+        self.ticket_components = tickets
 
     @property
     def ticket_components(self):
