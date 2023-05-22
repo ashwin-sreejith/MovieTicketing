c4fad7e671b09bc856cf269ce50ab23d7ddccccd
Ashwin Sreejith <ashwin.sreejith@gmail.com>
2023-05-22 15:14:17 +1000

    Added counter to track movie ID number

--- movie.py
+++ movie.py
@@ -1,6 +1,8 @@
 class Movie:
     """Class to handle movie entries"""
 
+    _MOVIE_ID_COUNT: int = 0
+
     def __init__(self, movie_id, movie_name, seats_available):
         self.movie_id = movie_id
         self.movie_name = movie_name
@@ -12,7 +14,7 @@ class Movie:
 
     @movie_name.setter
     def movie_name(self, movie_name):
-        self._movie_name = movie_name
+        self._movie_name = movie_name.capitalize()
 
     @property
     def movie_id(self):
@@ -20,7 +22,11 @@ class Movie:
 
     @movie_id.setter
     def movie_id(self, movie_id):
-        self._movie_id = movie_id
+        if len(movie_id) > 1:
+            self._movie_id = movie_id
+        else:
+            self._movie_id = movie_id + str(Movie._MOVIE_ID_COUNT)
+            Movie._MOVIE_ID_COUNT += 1
 
     @property
     def seats_available(self):
@@ -29,6 +35,14 @@ class Movie:
     @seats_available.setter
     def seats_available(self, seats: int):
         self._seats_available = int(seats)
+
+    @staticmethod
+    def get_customer_id_count():
+        return Movie._MOVIE_ID_COUNT
+
+    @staticmethod
+    def set_movie_id_count(movie_id_number: int):
+        Movie._MOVIE_ID_COUNT = movie_id_number
 
     def display_info(self):
         print(f"Movie ID : {self._movie_id}\n" + f"Movie Name : {self._movie_name}\n" +
