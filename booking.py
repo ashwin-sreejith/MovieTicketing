4b91a83f4187a35e9dfeb4f1a46ff9c795e16b87
Ashwin Sreejith <ashwin.sreejith@gmail.com>
2023-05-26 19:43:44 +1000

    tickets and quantities are of type list now

--- booking.py
+++ booking.py
@@ -6,11 +6,13 @@ class Booking:
         self._movie = movie
         self._ticket_type = ticket_type
         self._ticket_quantity = ticket_quantity
+        self.__ticket_cost = 0
 
     def compute_cost(self) -> tuple:
         """Computes all cost values including total cost, booking fee and discounts and returns a tuple"""
-        ticket_cost = self._ticket_type.ticket_price * self._ticket_quantity
-        booking_fee = self._customer.get_booking_fee(self._ticket_quantity)
-        discount = self._customer.get_discount(ticket_cost)
-        return ticket_cost, booking_fee, discount
+        for ticket, quantity in zip(self._ticket_type, self._ticket_quantity):
+            self.__ticket_cost += ticket.ticket_price * quantity
+        booking_fee = self._customer.get_booking_fee(sum(self._ticket_quantity))
+        discount = self._customer.get_discount(self.__ticket_cost)
+        return self.__ticket_cost, booking_fee, discount
 
